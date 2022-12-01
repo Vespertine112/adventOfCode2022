84e91bd296c2deeeb7256342d3b50c5ce54915a9
Brayden Hill <hillbgh@gmail.com>
2022-12-01 11:51:19 -0700

    Fixed sorting issue

--- day1/main.py
+++ day1/main.py
@@ -2,9 +2,8 @@ with open('input.txt') as f:
     lines = f.read().split("\n\n")               
     totals = []
     for group in lines:
-        numbers = [int(x) for x in group.split("\n")]
-        if len(totals) == 0 or sum(numbers) > totals[-1]: 
-            totals.append(sum(numbers))
+        totals.append(sum([int(x) for x in group.split("\n")]))
+    totals.sort()
 
 print(totals[-1])
 print(sum(totals[-3:]))
